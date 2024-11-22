e9abc9aded9fed200d1a67c21450db459a6c9307
PuceEdgar <puce.edgar@gmail.com>
2024-11-22 09:31:32 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Speech.Synthesis;
+namespace TTStest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			var tts = new SpeechSynthesizer();
+			tts.SetOutputToDefaultAudioDevice();
+			string message = Console.ReadLine();
+
+			while (!message.Equals("exit")) 
+			{
+				tts.Speak(message);
+				message = Console.ReadLine();
+			}
+
+
+			tts.Speak("bye");
+			Console.WriteLine("bye!");
+		}
+	}
+}
